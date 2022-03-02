2f7fbe2a65c3e180a0c0040a132b7a029d74d3f3
AdrianoLG-Medea <adriano.lorenzo@medea.es>
2022-03-02 18:04:40 +0100

    Hidden ACF form for Students

--- inc/register.php
+++ inc/register.php
@@ -5,6 +5,25 @@
  *
  * @subpackage k9umlaude
  */
+acf_form_head();
+
 $context = Timber::get_context();
+
+$context['form_args'] = [
+  'post_id' => 'new_post',
+  'post_title' => false,
+  'post_content' => false,
+  'post_custom_name' => true,
+  'field_groups' => ['group_621f4268813c3'], // ACF group id
+  'form' => true,
+  'return' => '/login', // Redirect to login once created
+  'html_before_fields' => '<div class="field">',
+  'html_after_fields' => '</div>',
+  'new_post' => [
+    'post_status' => 'draft',
+    'post_type' => 'student'
+  ],
+  'submit_value' => 'Crear estudiante'
+];
 
 Timber::render('register.twig', $context);
